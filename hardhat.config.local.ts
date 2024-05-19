6cb2e6fdb080ca2fbda2384b52bde98e9bfb5d96
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2024-05-18 23:53:29 -0300

    feat: creating dockerfile

---
+++ hardhat.config.local.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig, vars } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
+
+export default config;
