71ccb9860592dec5255b47f0e664b67723dd50e9
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2024-05-18 22:01:02 -0300

    feat: project config and modules

---
+++ README.md
@@ -0,0 +1,19 @@
+# STLHarvest Challenge
+
+Test to create STLHarvest offers a platform enabling users to deposit STL tokens to earn weekly
+incentives. Participants should have the flexibility to withdraw their initial investments
+and corresponding reward shares whenever they choose.
+
+## Tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat ignition deploy ./ignition/modules/Lock.ts
+```
+
+## Behavioral Diagram
+
+![STLHarvest Behavioral Diagram](./BehavioralDiagram.png)
---
+++ hardhat.config.ts
@@ -0,0 +1,36 @@
+import { HardhatUserConfig, vars } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const RPC_API_KEY = vars.get("RPC_API_KEY", "");
+
+const HR_TEAM_PRIVATE_KEY = vars.get(
+  "HR_TEAM_PRIVATE_KEY",
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+);
+
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY", "");
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${RPC_API_KEY}`,
+      accounts: [HR_TEAM_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+};
+
+export default config;
