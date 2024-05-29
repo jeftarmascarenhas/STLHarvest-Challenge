641900894e55f659c0b716bb6f35d3ef8cf63fd8
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2024-05-29 00:54:37 -0300

    feat: adding nat spec in STL Harvest Pool

--- contracts/STLHarvestPool.sol
+++ contracts/STLHarvestPool.sol
@@ -6,23 +6,53 @@ import "@openzeppelin/contracts/utils/Pausable.sol";
 import "./interfaces/ISTLHarvestPool.sol";
 import "./STL.sol";
 
+/**
+ * @title STLHarvestPool
+ * @author Jeftar Mascarenhas
+ */
 contract STLHarvestPool is ISTLHarvestPool, Ownable, Pausable {
+    /**
+     * stlToken store STL token address
+     */
     STL public immutable stlToken;
-
+    /**
+     * totalDeposits store users deposit amount
+     */
     uint256 public totalDeposits;
-    uint256 public lastDeposit;
-
+    /**
+     * Mapping investors is using to account investor information
+     */
     mapping(address account => Investor infoInvestor) public investors;
+    /**
+     * Mapping rewards is using to tracking reward date and amount
+     */
     mapping(uint256 createAt => uint256 amount) public rewards;
+    /**
+     * Mapping totalDepositTracking is using to tracking deposit date and amount
+     */
     mapping(uint256 createAt => uint256 amount) public totalDepositTracking;
-
+    /**
+     * totalRewards Total of reward
+     */
     uint256 public totalRewards;
+    /**
+     * lastReward is timestamp to store last reward amount added
+     * And help calc on function getUserRewards
+     */
     uint256 public lastReward;
 
+    /**
+     * Constructor
+     * @param _stlToken STL is token using to deposit and reward
+     */
     constructor(STL _stlToken) Ownable(_msgSender()) {
         stlToken = _stlToken;
     }
 
+    /**
+     * Function addRewards called only owner
+     * @param amount STL amount
+     */
     function addRewards(uint256 amount) external onlyOwner {
         stlToken.transferFrom(_msgSender(), address(this), amount);
 
@@ -34,6 +64,10 @@ contract STLHarvestPool is ISTLHarvestPool, Ownable, Pausable {
         emit RewardAdded(_msgSender(), amount);
     }
 
+    /**
+     * Function deposit to any user transfer STL token
+     * @param amount STL amount
+     */
     function deposit(uint256 amount) external {
         Investor storage newInvestor = investors[_msgSender()];
         newInvestor.amount += amount;
@@ -46,6 +80,9 @@ contract STLHarvestPool is ISTLHarvestPool, Ownable, Pausable {
         emit Deposited(_msgSender(), address(this), amount);
     }
 
+    /**
+     * Function withdraw to user get funds and rewards
+     */
     function withdraw() external {
         if (investors[_msgSender()].amount == 0) {
             revert InsufficientBalance(
@@ -64,6 +101,9 @@ contract STLHarvestPool is ISTLHarvestPool, Ownable, Pausable {
         emit Withdrawn(address(this), _msgSender(), amount);
     }
 
+    /**
+     * Function getUserRewards use to calc reward amount
+     */
     function getUserRewards() public view returns (uint256 amount) {
         if (investors[_msgSender()].amount == 0) {
             return amount;
